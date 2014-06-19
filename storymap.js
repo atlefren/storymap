b28449ee8314069900fb1f157d3849685d97ddde
Atle Frenvik Sveen <atle.sveen@gmail.com>
2014-06-19 19:28:02 +0200

    add content

---
+++ storymap.js
@@ -0,0 +1,130 @@
+(function ($) {
+    'use strict';
+
+
+    $.fn.storymap = function(options) {
+
+        var defaults = {
+            selector: '[data-place]',
+            breakpointPos: '33.333%',
+            createMap: function () {
+                // create a map in the "map" div, set the view to a given place and zoom
+                var map = L.map('map').setView([65, 18], 5);
+
+                // add an OpenStreetMap tile layer
+                L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+                    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                }).addTo(map);
+
+                return map;
+            }
+        };
+
+        var settings = $.extend(defaults, options);
+
+
+        if (typeof(L) === 'undefined') {
+            throw new Error('Storymap requires Laeaflet');
+        }
+        if (typeof(_) === 'undefined') {
+            throw new Error('Storymap requires underscore.js');
+        }
+
+        function getDistanceToTop(elem, top) {
+            var docViewTop = $(window).scrollTop();
+
+            var elemTop = $(elem).offset().top;
+
+            var dist = elemTop - docViewTop;
+
+            var d1 = top - dist;
+
+            if (d1 < 0) {
+                return $(window).height();
+            }
+            return d1;
+
+        }
+
+        function highlightTopPara(paragraphs, top) {
+
+            var distances = _.map(paragraphs, function (element) {
+                var dist = getDistanceToTop(element, top);
+                return {el: $(element), distance: dist};
+            });
+
+            var closest = _.min(distances, function (dist) {
+                return dist.distance;
+            });
+
+            _.each(paragraphs, function (element) {
+                var paragraph = $(element);
+                if (paragraph[0] !== closest.el[0]) {
+                    paragraph.trigger('notviewing');
+                }
+            });
+
+            if (!closest.el.hasClass('viewing')) {
+                closest.el.trigger('viewing');
+            }
+        }
+
+        function watchHighlight(element, searchfor, top) {
+            var paragraphs = element.find(searchfor);
+            highlightTopPara(paragraphs, top);
+            $(window).scroll(function () {
+                highlightTopPara(paragraphs, top);
+            });
+        }
+
+        var makeStoryMap = function (element, markers) {
+
+            var topElem = $('<div class="breakpoint-current"></div>')
+                .css('top', settings.breakpointPos);
+            $('body').append(topElem);
+
+            var top = topElem.offset().top - $(window).scrollTop();
+
+            var searchfor = settings.selector;
+
+            var paragraphs = element.find(searchfor);
+
+            paragraphs.on('viewing', function () {
+                $(this).addClass('viewing');
+            });
+
+            paragraphs.on('notviewing', function () {
+                $(this).removeClass('viewing');
+            });
+
+            watchHighlight(element, searchfor, top);
+
+            var map = settings.createMap();
+
+            var fg = L.featureGroup().addTo(map);
+
+            function showMapView(key) {
+
+                fg.clearLayers();
+                if (key === 'overview') {
+                    map.setView([65, 18], 5, true);
+                } else if (markers[key]) {
+                    var marker = markers[key];
+                    fg.addLayer(L.marker([marker.lat, marker.lon]));
+
+                    map.setView([marker.lat, marker.lon], marker.zoom, 1);
+                }
+
+            }
+
+            paragraphs.on('viewing', function () {
+                showMapView($(this).data('place'));
+            });
+        };
+
+        makeStoryMap(this, settings.markers);
+
+        return this;
+    }
+
+}(jQuery));
